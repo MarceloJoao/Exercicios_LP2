00bed44210ef16971ef7fb75a1c8d4ede34cafc4
Joao Marcelo <joaomarcelo586@gmail.com>
2023-09-07 20:09:44 -0300

    subindo atividade 3

---
+++ Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+
+Circle(float radius) {
+ this.radius = radius;
+
+}
+
+float radius;
+
+public double area (){
+ return(Math.PI * (radius * radius));
+}
+
+public double circumference(){
+    return(2 * Math.PI *radius);
+}
+
+}
